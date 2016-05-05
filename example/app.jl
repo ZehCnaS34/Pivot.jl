3e6c0004dc966f5459fa117935f263096af5acdd
Alexander Sanchez <asanchez.dev@gmail.com>
2016-05-05 12:45:52 -0400

    an example

---
+++ example/app.jl
@@ -0,0 +1,22 @@
+using Pivot
+
+app = Engine()
+
+use!(app, Pivot.Logger.simple)
+
+handle!(GET, app, "/") do ctx # might change this to context
+  "This should not make anything precompile if it is modified."
+end
+
+handle!(GET, app, "/:name") do ctx
+  "Name will be here $(ctx[:endpoint].captured) eventually"
+end
+
+handle!(GET, app, "/timed") do ctx
+  sleep(4)
+  "This should take some time."
+end
+
+println(app.root.root)
+
+Pivot.run(app, 8080)
