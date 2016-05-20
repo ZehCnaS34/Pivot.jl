19f86cb2338fab3eceba3b94a8ebaa0ea3c1b3d9
Alexander Sanchez <asanchez.dev@gmail.com>
2016-05-20 14:16:06 -0400

    moding the verbs

--- src/verbs.jl
+++ src/verbs.jl
@@ -1,10 +1,10 @@
 typealias Verb UInt8
 
-const GET    = 1  |> Verb
-const PUT    = 2  |> Verb
-const POST   = 4  |> Verb
-const DELETE = 8  |> Verb
-const PATCH  = 16 |> Verb
+const GET    = 1 << 0 |> Verb
+const PUT    = 1 << 1 |> Verb
+const POST   = 1 << 2 |> Verb
+const DELETE = 1 << 3 |> Verb
+const PATCH  = 1 << 4 |> Verb
 
 const STI = Dict(
   "GET"    =>  GET,
@@ -19,7 +19,6 @@ for cases when an endpoint can many different verbs
 """
 function proper_method(verb, map)
   in(verb, map |> keys) && return map[verb]
-
 
   for v in keys(map)
     verb & v > 0 && return map[v]
